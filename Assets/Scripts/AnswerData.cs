c9efe724bf0e7836116a299b9265307562e49bd0
janelle-nomad <janelle.saige@gmail.com>
2016-12-28 21:20:54 -0500

    Added the AnswerText, and isCorrect variable, with comments

--- Assets/Scripts/AnswerData.cs
+++ Assets/Scripts/AnswerData.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnswerData : MonoBehaviour {
+/*I can edit these values using the Unity editor within the "Inspector" tab */
+[System.Serializable] 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class AnswerData {
+
+
+    /** AnswerData class contains the answers to the Quiz Game's questions
+     * 
+     * The AnswerData class will not contain functions, 
+     * as such both the "Start" & "Update" functions will not be used
+     */
+
+    public string AnswerText; //Holds the string of the answer eg "Yes", "No", "Apple", "The Moon" etc
+
+                            // Since a bool only holds the value: true or false.
+    public bool isCorrect; // Contains the condition, is "User's answer" correct or not
+                           
 }
